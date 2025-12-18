fe31da5037602e9b437934c3fe1abed04cd3f640
milenapogon <pogonmilenka@gmail.com>
2025-12-18 17:59:23 +0100

    Add temperature converter and average grade features

--- python/main.py
+++ python/main.py
@@ -1,16 +1,19 @@
 while True:
     print("\n MENU ")
     print("1. Kalkulator")
+    print("2. Konwerter temperatur")
+    print("3. Średnia ocen ucznia")
     print("0. Wyjście")
 
-    wybor = input("Wybierz opcję: ")
+    wybor = input("Wybierz: ")
 
+    # ZADANIE 1
     if wybor == "1":
-        print("Zadanie 1 – Kalkulator")
+        print("\nZadanie 1 – Kalkulator")
 
         a = float(input("Podaj pierwszą liczbę: "))
         b = float(input("Podaj drugą liczbę: "))
-        op = input("Wybierz: +, -, *, /: ")
+        op = input("Wybierz (+, -, *, /): ")
 
         if op == "+":
             print("Wynik:", a + b)
@@ -26,6 +29,45 @@ while True:
         else:
             print("Nieznana operacja")
 
+    # ZADANIE 2
+    elif wybor == "2":
+        print("\nZadanie 2 – Konwerter temperatur")
+
+        kierunek = input("Wybierz kierunek C lub F: ")
+
+        if kierunek == "C":
+            c = float(input("Podaj temperaturę w °C: "))
+            f = c * 1.8 + 32
+            print(f"{c}°C = {f}°F")
+
+        elif kierunek == "F":
+            f = float(input("Podaj temperaturę w °F: "))
+            c = (f - 32) / 1.8
+            print(f"{f}°F = {c}°C")
+
+        else:
+            print("Nieprawidłowy wybór")
+
+    # ZADANIE 3
+    elif wybor == "3":
+        print("\nZadanie 3 – Średnia ocen ucznia")
+
+        liczba_ocen = int(input("Podaj liczbę ocen: "))
+        suma = 0
+
+        for i in range(liczba_ocen):
+            ocena = float(input("Podaj ocenę: "))
+            suma += ocena
+
+        srednia = suma / liczba_ocen
+        print("Średnia ocen:", round(srednia, 2))
+
+        if srednia >= 3.0:
+            print("Uczeń zdał.")
+        else:
+            print("Uczeń nie zdał.")
+
+    # WYJŚCIE
     elif wybor == "0":
         print("Koniec programu")
         break
